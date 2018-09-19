000c6c4b9b37a7c81101a5a7e6dd098496458536
Belinda Black <belinda.black@flatironschool.com>
2018-09-19 14:22:58 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog 
+	def initialize(name, breed)
+		@name = name
+		@breed = breed
+	end
+end
+
+fido = Dog.new("Fido", "Mutt")
+snoopy = Dog.new("Snoopy", "Cartoon")
+lassie = Dog.new("Lassie", "Shelty")
